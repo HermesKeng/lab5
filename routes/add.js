7b64c093cc7c4b2ed1887a57c3f3c8882da534b0
HermesKeng <kaining1101@gmail.com>
2021-02-04 10:36:38 -0800

    lab5

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,13 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
-	// Your code goes here
- }
+exports.addFriend = function (request, response) {
+  // Your code goes here
+  newFriend = {
+    name: request.query.name,
+    description: request.query.description,
+    imageURL: "https://loremflickr.com/500/500/people",
+  };
+  data.friends.push(newFriend);
+  console.log(request.query.name);
+  response.render("index", data);
+};
